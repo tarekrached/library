ffd6d38b90f0d3a0e076b6a9cb318d28ebb947b7
Tarek Rached <tarek.rached@gmail.com>
2020-10-27 11:23:05 -0700

    don't need to getAllFolders() bc we have cached folderIds

--- server/search.js
+++ server/search.js
@@ -67,43 +67,6 @@ async function fullSearch({drive, query, folderIds, results = [], nextPageToken:
   return total
 }
 
-// Grab all folders in directory to search through in shared drive
-async function getAllFolders({nextPageToken: pageToken, drive, parentIds = [driveId], foldersSoFar = []} = {}) {
-  const options = {
-    q: `(${parentIds.map((id) => `'${id}' in parents`).join(' or ')}) AND mimeType = 'application/vnd.google-apps.folder'`,
-    fields: 'files(id,name,mimeType,parents)'
-  }
-
-  if (pageToken) {
-    options.pageToken = pageToken
-  }
-
-  const {data} = await drive.files.list(options)
-
-  const {files, nextPageToken} = data
-  const combined = foldersSoFar.concat(files)
-
-  if (nextPageToken) {
-    return getAllFolders({
-      nextPageToken,
-      foldersSoFar: combined,
-      drive
-    })
-  }
-
-  const folders = combined.filter((item) => parentIds.includes(item.parents[0]))
-
-  if (folders.length > 0) {
-    return getAllFolders({
-      foldersSoFar: combined,
-      drive,
-      parentIds: folders.map((folder) => folder.id)
-    })
-  }
-
-  return combined.map((folder) => folder.id)
-}
-
 function getOptions(query, folderIds, driveType) {
   const fields = '*'
 
